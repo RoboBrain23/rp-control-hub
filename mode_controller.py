e2f26d04b39c9f3f158901febc738596bda840bc
Samy Nassar <samynassar996@gmail.com>
2023-06-27 01:15:20 +0300

    Initial Commit

---
+++ mode_controller.py
@@ -0,0 +1,8 @@
+"""
+mode_controller.py
+
+The `mode_controller.py` module is an integral part of the RP Control Hub system.
+It provides functionality for controlling and switching between different controlling modes.
+The module acts as a central hub for mode selection,
+allowing choosing between various control modes such as Gaze and BCI.
+"""
---
+++ streamer.py
@@ -0,0 +1,8 @@
+"""
+streamer.py
+
+The `streamer.py` module is a crucial component of the RP Control Hub system.
+It enables seamless streaming of sensor data from Raspberry Pi to a backend API.
+This module establishes a connection with the backend API and periodically sends sensor data,
+allowing real-time monitoring and analysis of the collected information.
+"""
