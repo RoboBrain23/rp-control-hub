75b7a197ef39de9121eb5e57fbaca2cd25c11ebe
Samy Nassar <samynassar996@gmail.com>
2023-06-29 09:27:18 +0300

    Sturcture the streamer class for multi-threading

--- streamer.py
+++ streamer.py
@@ -6,112 +6,83 @@ It enables seamless streaming of sensor data from Raspberry Pi to a backend API.
 This module establishes a connection with the backend API and periodically sends sensor data,
 allowing real-time monitoring and analysis of the collected information.
 """
-
 import requests
 import json
-import sched
 import time
 
-# URL of the endpoint to send the POST request
-URL = ""
-LOGIN_ENDPOINT = f"{URL}/chair/login"
-SENSOR_DATA_ENDPOINT = f"{URL}/chair/data"
-
-INTERVAL = 10  # in seconds
-
-CHAIR_ID = 3
-PASSWORD = "hahah"
-temperature = 36.5
-oximeter = 12.4
-pulse_rate = 19.8
-
-wheelchair = {
-    "chair_id": 3,
-    "password": "pass",
-    "temperature": 36.5,
-    "oximeter": 12.4,
-    "pulse_rate": 19.8
-
-}
-
-access_token = ""
-
-identity = {
-    "chair_id": wheelchair["chair_id"],
-    "password": wheelchair["password"],
-}
-
-# Set the headers for the request
-headers = {
-    "Content-Type": "application/json"
-}
+# from logger import app_logger
+# logger = app_logger
 
 
-# Login Function
-def login():
-    global headers
-    global access_token
+class Streamer:
+    def __init__(self, url, interval_in_seconds=7):
+        self.identity = {}
+        self.sensor_data = {}
+        self.access_token = ""
+        self.headers = {"Content-Type": "application/json"}
+        self.interval = interval_in_seconds
+        self.url = url
+        self.login_endpoint = f"{self.url}/chair/login"
+        self.sensor_data_endpoint = f"{self.url}/chair/data"
 
-    json_identity = json.dumps(identity)
-    response = requests.post(LOGIN_ENDPOINT, data=json_identity, headers=headers)
+    def login(self):
+        json_identity = json.dumps(self.identity)
+        response = requests.post(self.login_endpoint, data=json_identity, headers=self.headers)
 
-    if response.status_code == 200:
-        print("Sign-in successfully!. Status code:", response.status_code)
-        res = json.loads(response.text)
+        if response.status_code == 200:
+            print("Login successfully!. Status code:", response.status_code)
+            res = json.loads(response.text)
+            self.access_token = f"Bearer {res['access_token']}"
+            self.headers["Authorization"] = self.access_token
+            return response.status_code
+        else:
+            print("Sign-in Failed. Status code:", response.status_code)
+            print(response.text)
+            return response.status_code
 
-        access_token = f"Bearer {res['access_token']}"
+    def send_data(self, data):
+        req_body = data
 
-        headers = {
-            "Authorization": access_token
+        json_data = json.dumps(req_body)
+        response = requests.post(self.sensor_data_endpoint, data=json_data, headers=self.headers)
+
+        # Check the response status code
+        if response.status_code == 201:
+            print("Request sent successfully!. Status code:", response.status_code)
+            print(response.text)
+        elif response.status_code == 401:
+            print("Trying to login again")
+            self.login()
+        else:
+            print("Failed to send the request. Status code:", response.status_code)
+            print(response.text)
+
+    def get_identity(self):
+        return self.identity
+
+    def set_identity(self, chair_id, password):
+        self.identity["chair_id"] = chair_id
+        self.identity["password"] = password
+
+    def get_sensor_data(self):
+        return self.sensor_data
+
+    def set_sensor_data(self, data):
+        self.sensor_data = {
+            "temperature": data["temperature"],
+            "oximeter": data["oximeter"],
+            "pulse_rate": data["pulse_rate"]
         }
 
-        return response.status_code
-    else:
-        print("Sign-in Failed. Status code:", response.status_code)
-        print(response.text)
-        return response.status_code
+    def set_url(self, url: str):
+        self.url = url
 
+    def stream(self):
+        login_status_code = self.login()
 
-# Send the POST request
-def send_data():
-    data = {
-        "chair_id": wheelchair["chair_id"],
-        "temperature": wheelchair["temperature"],
-        "oximeter": wheelchair["oximeter"],
-        "pulse_rate": wheelchair["pulse_rate"]
-    }
-
-    json_data = json.dumps(data)
-    response = requests.post(SENSOR_DATA_ENDPOINT, data=json_data, headers=headers)
-
-    print(f"HEADER: {headers}")
-
-    # Check the response status code
-    if response.status_code == 201:
-        print("Request sent successfully!. Status code:", response.status_code)
-        print(response.text)
-    elif response.status_code == 401:
-        print("Trying to login again")
-        login()
-    else:
-        print("Failed to send the request. Status code:", response.status_code)
-        print(response.text)
-
-
-def main():
-    login_status_code = login()
-
-    if login_status_code == 200:
-        scheduler = sched.scheduler(time.time, time.sleep)
-        scheduler.enter(0, 1, send_data)
-        interval = INTERVAL
-
-        while True:
-            scheduler.enter(interval, 1, send_data)
-            scheduler.run()
-    else:
-        print("Failed to send the request.")
-
-
-if __name__ == '__main__':
-    main()
+        if login_status_code == 200:
+            while True:
+                self.send_data(self.sensor_data)
+                time.sleep(self.interval)
+        else:
+            print("Failed to send the request.")
