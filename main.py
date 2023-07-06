e9fe72d3cc2220b994b06d2cbb7e44c8e20acc8e
Samy Nassar <samynassar996@gmail.com>
2023-07-06 23:13:58 +0300

    Integrate gaze module and send data to arduino

--- main.py
+++ main.py
@@ -2,23 +2,40 @@ import time
 import streamer
 import random
 import threading
-import multiprocessing
+from multiprocessing import Process, Manager, Value
 from queue import Queue
+import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(ROOT_DIR, 'Gaze'))
 
 from logger import app_logger
+from Gaze import main as gaze
 
 logger = app_logger
 
 URL = "http://localhost:3000"
 STREAMER_INTERVAL = 5
-DATA_LISTENER_INTERVAL = 3
+GET_ARDUINO_DATA_INTERVAL = 3
+SEND_ARDUINO_DATA_INTERVAL = 0.1
 MODE_RESPONSE_INTERNAL = 1
 
-NO_FLAG = -1
+NO_FLAG = 0
 GAZE_FLAG = 1
 BCI_FLAG = 2
 
-direction = ""
+# GAZE Constants
+gaze_constants = {
+    'STOP': -1,
+    'NO_MOVEMENT': 0,
+    'FORWARD': 1,
+    'RIGHT': 2,
+    'LEFT': 3
+}
+
+
+direction = Value('u', 'S')
 
 data = {
     "chair_id": "5",
@@ -34,7 +51,7 @@ lock = threading.Lock()
 streamer = streamer.Streamer(URL, STREAMER_INTERVAL)
 
 
-def update_data():
+def get_arduino_data():
     global data
     while True:
         with lock:
@@ -43,8 +60,18 @@ def update_data():
             data["temperature"] = random.randint(1, 100)
             data["oximeter"] = random.randint(1, 100)
             data["pulse_rate"] = random.randint(1, 100)
-            data["flag"] = random.randint(-1, 2)
-        time.sleep(DATA_LISTENER_INTERVAL)
+            # data["flag"] = random.randint(0, 2)
+            data["flag"] = 1
+        time.sleep(GET_ARDUINO_DATA_INTERVAL)
+
+
+def send_arduino_data():
+    global direction
+    while True:
+        if direction.value != 'p':
+            print(f"SENDING TO ARDUINO : {direction.value}")
+
+        time.sleep(SEND_ARDUINO_DATA_INTERVAL)
 
 
 def stream():
@@ -55,7 +82,7 @@ def stream():
         if data["chair_id"] != "" and data["password"] != "":
             has_id = True
         time.sleep(1)
-        logger.warning(f"Chair has no id or password, chair_id: '{data['chair_id']}', password: '{data['password']}'")
+        # logger.warning(f"Chair has no id or password, chair_id: '{data['chair_id']}', password: '{data['password']}'")
 
     while True:
         d = {
@@ -67,20 +94,22 @@ def stream():
             streamer.send_data(d)
             time.sleep(streamer.interval)
         except:
-            logger.error(f"Can't stream this data: {d}")
+            # logger.error(f"Can't stream this data: {d}")
+            continue
 
 
-def run_gaze():
+def run_gaze(direct):
     # TODO: IMPLEMENT GAZE CONTROL.
-    global direction
-    while True:
-        logger.info("GAZE is running...")
-        time.sleep(1)
+    # global direction
+    # while True:
+    #     logger.info("GAZE is running...")
+    #     time.sleep(1)
+    gaze.run_gaze(direct)
 
 
 def run_bci():
     # TODO: IMPLEMENT BCI CONTROL.
-    global direction
+    # global direction
     while True:
         logger.info("BCI is running...")
         time.sleep(1)
@@ -90,12 +119,17 @@ if __name__ == '__main__':
 
     current_flag = data['flag']
 
-    t1 = threading.Thread(target=update_data, name="data_listener_thread", daemon=True)
+    t1 = threading.Thread(target=get_arduino_data, name="data_listener_thread", daemon=True)
     t2 = threading.Thread(target=stream, name="streamer_thread", daemon=True)
+    t3 = threading.Thread(target=send_arduino_data, name="send_arduino_data_thread", daemon=True)
 
     t1.start()
     t2.start()
+    t3.start()
 
+    manger = Manager()
+    # direction = manger.dict({'direction': gaze_constants['NO_MOVEMENT']})
+    # direction = Value('i', gaze_constants['NO_MOVEMENT'])
     process_queue = Queue(maxsize=1)
 
     while True:
@@ -112,7 +146,7 @@ if __name__ == '__main__':
                 if not process_queue.empty():
                     p = process_queue.get()
                     p.terminate()
-                gaze_process = multiprocessing.Process(target=run_gaze, name="GazeProcess")
+                gaze_process = Process(target=run_gaze, args=(direction, ), name="GazeProcess")
                 process_queue.put(gaze_process)
                 gaze_process.start()
                 current_flag = GAZE_FLAG
@@ -121,12 +155,12 @@ if __name__ == '__main__':
                 if not process_queue.empty():
                     p = process_queue.get()
                     p.terminate()
-                bci_process = multiprocessing.Process(target=run_gaze, name="BciProcess")
+                bci_process = Process(target=run_bci, name="BciProcess")
                 process_queue.put(bci_process)
                 bci_process.start()
                 current_flag = BCI_FLAG
         else:
             logger.warning(f"UNKNOWN DESIRED FLAG: {desired_flag}!")
 
-        logger.info(f"Current flag is: {current_flag}")
+        # logger.info(f"Current flag is: {current_flag}")
         time.sleep(MODE_RESPONSE_INTERNAL)
